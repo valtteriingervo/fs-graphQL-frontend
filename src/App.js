00537af9d1c288bf477850b3c591ebd3a70339f9
Valtteri Ingervo <valtteri.ingervo@aalto.fi>
2023-04-10 09:21:27 +0300

    8.8

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import { useState } from 'react'
+import { useQuery } from '@apollo/client'
+
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 
+import { ALL_AUTHORS } from './queries'
+
 const App = () => {
   const [page, setPage] = useState('authors')
+  const result = useQuery(ALL_AUTHORS)
+
+  if (result.loading) {
+    return <div>loading ...</div>
+  }
 
   return (
     <div>
@@ -14,7 +23,7 @@ const App = () => {
         <button onClick={() => setPage('add')}>add book</button>
       </div>
 
-      <Authors show={page === 'authors'} />
+      <Authors show={page === 'authors'} authors={result.data.allAuthors} />
 
       <Books show={page === 'books'} />
 
